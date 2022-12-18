a5b73481a6972aaf8313fd037a65c41d37f4a635
Bhushaan Shinde <root@ip-172-31-4-17.ap-south-1.compute.internal>
2022-12-18 14:11:50 +0000

    Updates in jenkins_ubuntu script.

--- Jenkins/jenkins_ubuntu.sh
+++ Jenkins/jenkins_ubuntu.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-sudo su
-apt-get update -y
-apt install openjdk-8-jre -y
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-apt-get update -y
+apt update
+
+sudo apt install openjdk-11-jre
+
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+apt-get update
+apt-get upgrade
+
 apt-get install jenkins -y
 sudo systemctl start jenkins
 sudo systemctl enable jenkins
 sudo systemctl status jenkins
 
+cat /var/lib/jenkins/secrets/initialAdminPassword
